6a097717357ed1023cb4adc79c0b8c09a12d85bb
Brian Floersch <gh123man@gmail.com>
2015-07-26 13:23:43 -0400

    created custom chart

--- src/main/java/com/ghsoft/barometergraph/fragments/LiveGraphFragment.java
+++ src/main/java/com/ghsoft/barometergraph/fragments/LiveGraphFragment.java
@@ -2,32 +2,23 @@ package com.ghsoft.barometergraph.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ghsoft.barometergraph.R;
-import com.ghsoft.barometergraph.data.BarometerDataPoint;
-import com.ghsoft.barometergraph.data.IDataReceiver;
 import com.ghsoft.barometergraph.service.BarometerDataService;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-import java.util.LinkedList;
+import com.ghsoft.barometergraph.views.BarometerDataGraph;
 
 /**
  * Created by brian on 7/21/15.
  */
-public class LiveGraphFragment extends Fragment implements IDataReceiver {
+public class LiveGraphFragment extends Fragment {
 
     private BarometerDataService mService;
     private LiveGraphFragmentEvents mEvents;
-    private LineChart mChart;
+    private BarometerDataGraph mChart;
     private boolean mLiveWrite;
 
     public LiveGraphFragment() {
@@ -50,94 +41,13 @@ public class LiveGraphFragment extends Fragment implements IDataReceiver {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_live_graph, null);
-        mChart = (LineChart) v.findViewById(R.id.chart);
-        LineData data = new LineData();
-        //data.setValueTextColor(Color.WHITE);
-        mChart.setData(data);
-        mChart.getAxisRight().setStartAtZero(false);
-        mChart.getAxisRight().setDrawLabels(false);
-        mChart.getAxisLeft().setStartAtZero(false);
-        mChart.setAutoScaleMinMaxEnabled(true);
-       // mChart.setVisibleYRangeMaximum(1f, YAxis.AxisDependency.RIGHT);
+        mChart = (BarometerDataGraph) v.findViewById(R.id.chart);
         return v;
-    }
-
-    @Override
-    public void writeHistory(LinkedList<BarometerDataPoint> data) {
-
-        LineData lineData = mChart.getData();
-
-        if (data != null) {
-
-            LineDataSet set = lineData.getDataSetByIndex(0);
-
-            if (set == null) {
-                set = createSet();
-                lineData.addDataSet(set);
-            }
-
-            for (BarometerDataPoint dataPoint : data) {
-                lineData.addXValue("" + dataPoint.getmTimeStamp());
-                lineData.addEntry(new Entry(dataPoint.getValue(), set.getEntryCount()), 0);
-            }
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(150);
-            mChart.moveViewToX(lineData.getXValCount() - 151);
-        }
-        mLiveWrite = true;
-    }
-
-    @Override
-    public void write(BarometerDataPoint dataPoint) {
-        if (!mLiveWrite)
-            return;
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            LineDataSet set = data.getDataSetByIndex(0);
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-            // add a new x-value first
-            data.addXValue("" + dataPoint.getmTimeStamp());
-            data.addEntry(new Entry(dataPoint.getValue(), set.getEntryCount()), 0);
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(150);
-            mChart.moveViewToX(data.getXValCount() - 151);
-        }
-    }
-
-    private LineDataSet createSet() {
-
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
-        set.setColor(ColorTemplate.getHoloBlue());
-        set.setLineWidth(2f);
-        set.setDrawCircles(false);
-        set.setFillAlpha(65);
-        set.setFillColor(ColorTemplate.getHoloBlue());
-        set.setHighLightColor(Color.rgb(244, 117, 117));
-        set.setValueTextColor(Color.WHITE);
-        set.setValueTextSize(9f);
-        set.setDrawValues(false);
-        return set;
-    }
+   }
 
     public void setService(BarometerDataService service) {
         mService = service;
-        mService.setDataReceiver(this);
+        mService.setDataReceiver(mChart);
     }
 
 
--- src/main/java/com/ghsoft/barometergraph/views/BarometerDataGraph.java
+++ src/main/java/com/ghsoft/barometergraph/views/BarometerDataGraph.java
@@ -1,15 +1,111 @@
 package com.ghsoft.barometergraph.views;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
 
+import com.ghsoft.barometergraph.data.BarometerDataPoint;
+import com.ghsoft.barometergraph.data.IDataReceiver;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.LinkedList;
 
 /**
  * Created by brian on 7/26/15.
  */
-public class BarometerDataGraph extends LineChart {
+public class BarometerDataGraph extends LineChart implements IDataReceiver {
 
     public BarometerDataGraph(Context context) {
         super(context);
+        setupView(context);
+    }
+
+    public BarometerDataGraph(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupView(context);
+    }
+
+    public BarometerDataGraph(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setupView(context);
+    }
+
+    private void setupView(Context context) {
+        LineData data = new LineData();
+        setData(data);
+        getAxisRight().setStartAtZero(false);
+        getAxisRight().setDrawLabels(false);
+        getAxisLeft().setStartAtZero(false);
+        setAutoScaleMinMaxEnabled(true);
+    }
+
+    private LineDataSet getDataSet(LineData data) {
+        LineDataSet set = data.getDataSetByIndex(0);
+
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+        return set;
+    }
+
+    private void scrollToFront(LineData data) {
+        setVisibleXRangeMaximum(150);
+        moveViewToX(data.getXValCount() - 151);
+    }
+
+    @Override
+    public void write(BarometerDataPoint dataPoint) {
+        LineData lineData = getData();
+
+        if (lineData == null) return;
+
+        LineDataSet set = getDataSet(lineData);
+
+        // add a new x-value first
+        lineData.addXValue("" + dataPoint.getmTimeStamp());
+        lineData.addEntry(new Entry(dataPoint.getValue(), set.getEntryCount()), 0);
+
+        // let the chart know it's data has changed
+        notifyDataSetChanged();
+        scrollToFront(lineData);
+
+    }
+
+    @Override
+    public void writeHistory(LinkedList<BarometerDataPoint> data) {
+
+        LineData lineData = getData();
+
+        if (data == null) return;
+
+        LineDataSet set = getDataSet(lineData);
+
+        for (BarometerDataPoint dataPoint : data) {
+            lineData.addXValue("" + dataPoint.getmTimeStamp());
+            lineData.addEntry(new Entry(dataPoint.getValue(), set.getEntryCount()), 0);
+        }
+
+        notifyDataSetChanged();
+
+        scrollToFront(lineData);
+    }
+
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setLineWidth(2f);
+        set.setDrawCircles(false);
+        set.setFillAlpha(65);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+        set.setDrawValues(false);
+        return set;
     }
 }
